d7caec9562600b6571ceffcbbf6b05bd7e4bc3e4
Alec Wells <alec.wells47@gmail.com>
2019-07-01 17:55:30 -0600

    Add setup method
    Co-authored-by: Evette <50186721+evettetelyas@users.noreply.github.com>

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -6,56 +6,53 @@ require 'pry'
 
 class ShipTest < Minitest::Test
 
-  def test_it_exists
-    cruiser = Ship.new("Cruiser", 3)
+  def setup
+    @cruiser = Ship.new("Cruiser", 3)
+  end
 
-    assert_instance_of Ship, cruiser
+  def test_it_exists
+
+    assert_instance_of Ship, @cruiser
   end
 
   def test_it_has_a_name
-    cruiser = Ship.new("Cruiser", 3)
 
-    assert_equal "Cruiser", cruiser.name
+    assert_equal "Cruiser", @cruiser.name
   end
 
   def test_it_has_a_length
-    cruiser = Ship.new("Cruiser", 3)
 
-    assert_equal 3, cruiser.length
+    assert_equal 3, @cruiser.length
   end
 
   def test_it_has_health
-    cruiser = Ship.new("Cruiser", 3)
 
-    assert_equal 3, cruiser.health
+    assert_equal 3, @cruiser.health
   end
 
   def test_it_returns_false_as_default_for_sunk?
-    cruiser = Ship.new("Cruiser", 3)
 
-    refute cruiser.sunk?
+    refute @cruiser.sunk?
   end
 
   def test_it_has_hit_method_and_changes_health_dynamically
-    cruiser = Ship.new("Cruiser", 3)
-    cruiser.hit
+    @cruiser.hit
 
-    assert_equal 2, cruiser.health
+    assert_equal 2, @cruiser.health
 
-    cruiser.hit
-    assert_equal 1, cruiser.health
+    @cruiser.hit
+    assert_equal 1, @cruiser.health
 
-    cruiser.hit
-    assert_equal 0, cruiser.health
+    @cruiser.hit
+    assert_equal 0, @cruiser.health
   end
 
   def test_it_can_be_sunk_if_hit_enough
-    cruiser = Ship.new("Cruiser", 3)
-    cruiser.hit
-    cruiser.hit
-    cruiser.hit
+    @cruiser.hit
+    @cruiser.hit
+    @cruiser.hit
 
-    assert cruiser.sunk?
+    assert @cruiser.sunk?
   end
 
 end
