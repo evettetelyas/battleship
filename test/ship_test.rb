b142324bfe4ad8970f52491bda511d4cb155d51b
Alec Wells <alec.wells47@gmail.com>
2019-07-01 17:18:35 -0600

    Add instance and test
    Co-authored-by: Evette <50186721+evettetelyas@users.noreply.github.com>

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -3,30 +3,44 @@ require 'minitest/pride'
 require './lib/ship'
 require 'pry'
 
+
 class ShipTest < Minitest::Test
 
   def test_it_exists
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_instance_of Ship, cruiser
   end
 
   def test_it_has_a_name
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal "Cruiser", cruiser.name
   end
 
   def test_it_has_a_length
+    skip
+
   end
 
   def test_it_has_health
+    skip
   end
 
   def test_it_returns_false_as_default_for_sunk?
+    skip
   end
 
   def test_it_has_hit_method
+    skip
   end
 
   def test_hit_changes_health
+    skip
   end
 
   def test_it_can_be_sunk_if_hit_enough
+    skip
   end
 
 
