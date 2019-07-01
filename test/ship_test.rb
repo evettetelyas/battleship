91a21e1c2873b294f47df0db9d3df43f69dc9a74
Alec Wells <alec.wells47@gmail.com>
2019-07-01 17:10:11 -0600

    Create ship and name tests
    Co-authored-by: Evette <50186721+evettetelyas@users.noreply.github.com>

---
+++ test/ship_test.rb
@@ -0,0 +1,33 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require 'pry'
+
+class ShipTest < Minitest::Test
+
+  def test_it_exists
+  end
+
+  def test_it_has_a_name
+  end
+
+  def test_it_has_a_length
+  end
+
+  def test_it_has_health
+  end
+
+  def test_it_returns_false_as_default_for_sunk?
+  end
+
+  def test_it_has_hit_method
+  end
+
+  def test_hit_changes_health
+  end
+
+  def test_it_can_be_sunk_if_hit_enough
+  end
+
+
+end
