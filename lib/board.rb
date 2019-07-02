3d42f6eb14d1d4b1edb87fe3627f858d10861032
Alec Wells <alec.wells47@gmail.com>
2019-07-02 16:14:02 -0600

    Start valid placement
    Co-authored-by: Evette <50186721+evettetelyas@users.noreply.github.com>

--- lib/board.rb
+++ lib/board.rb
@@ -9,4 +9,14 @@ class Board
     @cells.keys.include?(coordinate)
   end
 
+  def valid_placement?(ship, coordinates)
+    if ship.length == coordinates.count
+      true
+    else
+      false
+    end
+
+    
+  end
+
 end
