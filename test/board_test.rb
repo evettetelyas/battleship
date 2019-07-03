616d26fa1db00d1d3004ed0082890be0b5650752
Alec Wells <alec.wells47@gmail.com>
2019-07-03 07:04:44 -0600

    Commit for safety
    Co-authored-by: Evette <50186721+evettetelyas@users.noreply.github.com>

--- test/board_test.rb
+++ test/board_test.rb
@@ -43,114 +43,77 @@ class BoardTest < Minitest::Test
   end
 
   def test_if_coordinate_is_valid
-
     assert @board.valid_coordinate(:A1)
-
     assert @board.valid_coordinate(:D4)
-
     refute @board.valid_coordinate(:A5)
-
     refute @board.valid_coordinate(:E1)
-
     refute @board.valid_coordinate(:A22)
   end
 
   def test_valid_placement_for_ship
     refute @board.valid_placement?(@cruiser, [:A3, :A2, :A1])
-
     refute @board.valid_placement?(@cruiser, [:A1, :A2, :A4])
-
     assert @board.valid_placement?(@cruiser, [:A1, :A2, :A3])
-
     refute @board.valid_placement?(@submarine, [:A1, :C1])
-
     refute @board.valid_placement?(@submarine, [:C1, :B1])
-
     refute @board.valid_placement?(@cruiser, [:A1, :B2, :C3])
-
     refute @board.valid_placement?(@submarine, [:C2, :D3])
-
     assert @board.valid_placement?(@cruiser, [:B1, :C1, :D1])
-
     assert @board.valid_placement?(@submarine, [:A1, :A2])
   end
 
   def test_all_letters_of_coordinates_same
     coordinates_1 = [:A1, :A2, :A3]
-
     assert @board.all_letters_same?(coordinates_1)
-
     coordinates_2 = [:A1, :A2, :A4]
-
     assert @board.all_letters_same?(coordinates_2)
-
     coordinates_3 = [:A1, :B2, :A4]
-
     refute @board.all_letters_same?(coordinates_3)
   end
 
   def test_all_letters_uniq
     coordinates_1 = [:A1, :B2, :C3, :D4]
-
     assert @board.all_letters_uniq?(coordinates_1)
-
     coordinates_2 = [:A1, :B2, :B3, :D4]
-
     refute @board.all_letters_uniq?(coordinates_2)
   end
 
   def test_all_numbers_are_same
     coordinates_1 = [:A1, :B1, :C1]
-
     assert @board.all_numbers_same?(coordinates_1)
-
     coordinates_2 = [:A1, :B2, :C3]
-
     refute @board.all_numbers_same?(coordinates_2)
-
     coordinates_3 = [:A1, :B1, :D1]
-
     assert @board.all_numbers_same?(coordinates_3)
-
     coordinates_4 = [:A3, :B3, :C3]
-
     assert @board.all_numbers_same?(coordinates_4)
   end
 
   def test_numbers_are_sequential
     coordinates_1 = [:A1, :A2, :A3]
-
     assert @board.numbers_sequential?(coordinates_1)
-
     coordinates_2 = [:A1, :A2, :A4]
-
     refute @board.numbers_sequential?(coordinates_2)
-
     coordinates_3 = [:A1, :B2, :C3]
-
     assert @board.numbers_sequential?(coordinates_3)
-
     coordinates_4 = [:A3, :B2, :C1]
-
     refute @board.numbers_sequential?(coordinates_4)
   end
 
   def test_letters_are_sequential
     coordinates_1 = [:A1, :A2, :A3]
-
     refute @board.letters_sequential?(coordinates_1)
-
     coordinates_2 = [:A1, :B2, :C3]
-
     assert @board.letters_sequential?(coordinates_2)
-
     coordinates_3 = [:A1, :B1, :C1, :D1, :E1]
-
     assert @board.letters_sequential?(coordinates_3)
-
     coordinates_4 = [:E1, :D1, :C1, :B1, :A1]
-
     refute @board.letters_sequential?(coordinates_4)
+  end
+
+  def test_place_ship
+    skip
+
   end
 
 end
