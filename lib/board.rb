e3cc361d929849eb279b1a3c468d1ea39ea313dc
Evette Telyas <etelyas@gmail.com>
2019-07-04 16:57:40 -0600

    complete board.render (hardcoded until iteration 4)
    Co-authored-by: Alec Wells <23268508+alect47@users.noreply.github.com>

--- lib/board.rb
+++ lib/board.rb
@@ -83,37 +83,37 @@ class Board
   end
 
   def render
-    split_coordinates = @cells.keys.map {|coordinate|
-        coordinate.to_s.chars}
+    # split_coordinates = @cells.keys.map {|coordinate|
+    #     coordinate.to_s.chars}
+    #
+    # num_ary = split_coordinates.map {|c| c[1]}.uniq
+    # num_times = num_ary.sort.last.to_i
+    # # letter_ary = split_coordinates.map {|c| c[0]}.uniq
+    # id_strings = @cells.keys.collect {|symbol| symbol.to_s}
+    #
+    # render_output = "  "
+    #
+    # render_output += (num_ary.join(" ") + " \n") + "A "
+    #
+    # row_output = num_times.times do
+    #   @cells.each do |cell|
+    #     p @cells[].render
+    #   end
+    # end
+    #
+    # a_spaces = id_strings.select {|id| id.include?("A")}
+    # a_spaces_symbols = a_spaces.map {|id| id.to_sym}
+    #
+    # b_spaces = id_strings.select {|id| id.include?("B")}
+    # b_spaces_symbols = b_spaces.map {|id| id.to_sym}
+    #
+    # c_spaces = id_strings.select {|id| id.include?("C")}
+    # c_spaces_symbols = c_spaces.map {|id| id.to_sym}
+    #
+    # d_spaces = id_strings.select {|id| id.include?("D")}
+    # d_spaces_symbols = d_spaces.map {|id| id.to_sym}
 
-    num_ary = split_coordinates.map {|c| c[1]}.uniq
-    num_times = num_ary.sort.last.to_i
-    # letter_ary = split_coordinates.map {|c| c[0]}.uniq
-    id_strings = @cells.keys.collect {|symbol| symbol.to_s}
-
-    render_output = "  "
-
-    render_output += (num_ary.join(" ") + " \n") + "A "
-
-    row_output = num_times.times do
-      @cells.each do |cell|
-        p @cells[].render
-      end
-    end
-
-    a_spaces = id_strings.select {|id| id.include?("A")}
-    a_spaces_symbols = a_spaces.map {|id| id.to_sym}
-
-    b_spaces = id_strings.select {|id| id.include?("B")}
-    b_spaces_symbols = b_spaces.map {|id| id.to_sym}
-
-    c_spaces = id_strings.select {|id| id.include?("C")}
-    c_spaces_symbols = c_spaces.map {|id| id.to_sym}
-
-    d_spaces = id_strings.select {|id| id.include?("D")}
-    d_spaces_symbols = d_spaces.map {|id| id.to_sym}
-
-
+    return "  1 2 3 4 \nA #{@cells[:A1].render} #{@cells[:A2].render} #{@cells[:A3].render} #{@cells[:A4].render} \nB #{@cells[:B1].render} #{@cells[:B2].render} #{@cells[:B3].render} #{@cells[:B4].render} \nC #{@cells[:C1].render} #{@cells[:C2].render} #{@cells[:C3].render} #{@cells[:C4].render} \nD #{@cells[:D1].render} #{@cells[:D2].render} #{@cells[:D3].render} #{@cells[:D4].render} \n"
   end
 
 
