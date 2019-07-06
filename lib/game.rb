caf535d969f86ab23e9ad05f228e6edaec9c37fe
Evette Telyas <etelyas@gmail.com>
2019-07-06 17:54:00 -0600

    place player ships
    Co-authored-by: Alec Wells <23268508+alect47@users.noreply.github.com>

--- lib/game.rb
+++ lib/game.rb
@@ -22,10 +22,37 @@ def start
 end
 
 def place_all_comp_ships
-  # @comp_board.place_comp_ship(@comp_ships[:cruiser])
-comp_ships.keys.each {|key| @comp_board.place_comp_ship(@comp_ships[key])}
-
+  comp_ships.keys.each {|key| @comp_board.place_comp_ship(@comp_ships[key])}
 end
 
+def place_player_cruiser
+  puts "I have laid out my ships on the grid. You now need to lay your two ships. The Cruiser is three units long, and the Submarine is two uits long."
+  puts @player_board.render
+  puts "Enter the squares for your Cruiser (3 spaces)"
+  print "> "
+
+  coordinates = (gets.chomp.upcase.split(" ")).map {|cell| cell.to_sym}
+
+  until @player_board.valid_placement?(@player_ships[:cruiser], coordinates)
+    puts "Those are invalid coordinates. Please try again:"
+    coordinates = (gets.chomp.upcase.split(" ")).map {|cell| cell.to_sym}
+  end
+  @player_board.place(@player_ships[:cruiser], coordinates)
+  puts @player_board.render
+end
+
+def place_player_submarine
+  puts "Enter the squares for your Submarine (2 spaces)"
+  print "> "
+
+  coordinates = (gets.chomp.upcase.split(" ")).map {|cell| cell.to_sym}
+
+  until @player_board.valid_placement?(@player_ships[:submarine], coordinates)
+    puts "Those are invalid coordinates. Please try again:"
+    coordinates = (gets.chomp.upcase.split(" ")).map {|cell| cell.to_sym}
+  end
+  @player_board.place(@player_ships[:submarine], coordinates)
+  puts @player_board.render
+end
 
 end
